8bff38e0d84678b6472b764efbe7578bca7773da
kuith <kuithsteam@gmail.com>
2022-04-12 21:43:02 +0200

    Corrección faltas de ortografía

--- src/util/datos/datosContacto.js
+++ src/util/datos/datosContacto.js
@@ -1,15 +1,15 @@
 const contacto = {
   titulo:"Contacto",
   calle:"Ronda de Saliente, nº 1",
-  ciudad:"Torrejon de Ardoz (Madrid).",
+  ciudad:"Torrejón de Ardoz (Madrid).",
   telefono:"91 011 12 60 - 653 82 29 45",
   correo:"josesevilla.herrera@hotmail.com",
   horario:"Horario",
   horarioSemana:"De lunes a viernes de 08:00 a 14:00 y de 16:00 a 21:00",
-  horarioSabado:"Sabados de 08:00 a 14:00",
-  aviso:"En caso de no avisar o no acudir a la sesion se le restara del bono.",
-  pedir:"Pedir citas previas por telefono o whatsapp.",
-  parking:"1 hora de aparcamiento gratuito en parking publico de la Plaza mayor (Consultar)."
+  horarioSabado:"Sábados de 08:00 a 14:00",
+  aviso:"En caso de no avisar o no acudir a la sesión se le restará del bono.",
+  pedir:"Pedir citas previas por teléfono o whatsapp.",
+  parking:"1 hora de aparcamiento gratuito en parking público de la Plaza mayor (consultar)."
 }
 export {contacto}
 
